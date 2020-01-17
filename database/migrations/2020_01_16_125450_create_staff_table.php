a42ca2e640c86feae1af7491ad91cf58add24b23
sinthujacsc <sinthu96vijaya@gmail.com>
2020-01-17 15:52:54 +0000

    added files

---
+++ database/migrations/2020_01_16_125450_create_staff_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->String('staff_id',10);
+            $table->String('staff_name',30);
+            $table->String('languages',20);
+            $table->String('password',20);
+            $table->integer('mobile_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
---
+++ database/migrations/2020_01_16_143226_create_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('student_id');
+            $table->String('student_name',20);
+            $table->String('status');
+            $table->String('email_id');
+            $table->String('address');
+            $table->integer('phone_number');
+            $table->String('course_id',10);
+
+            $table->date('end_by');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
