31e97b1b1fae74040ddc28c1fe47998d1ab4c703
cknadeeshan <nadeeshanck@gmail.com>
2020-01-23 16:44:22 +0530

    some changes in edit page

--- resources/views/inquiry/edit.blade.php
+++ resources/views/inquiry/edit.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.master')
 @section('content')
-<div>
-<a href ="{{URL::previous()}}" button type="button" class="btn btn-primary">Back</a>
-</div>
-<br>
+
 
 <form action = "{{route('inquiry.update',$inquiry->id)}}" method = "POST">
   <input type = "hidden" name = "_method" value="put">
      {{csrf_field()}}
-   <div class="col-md-6">
-            <!-- general form elements disabled -->
-            <div class="card card-warning">
+   
+
+<div class=" d-flex justify-content-center  ">
+                <div class=" col-8  ">
+            <div class="card card-primary">
               <div class="card-header">
                 <h3 class="card-title">Edit</h3>
               </div>
@@ -21,51 +20,89 @@
                     <div class="col-sm-6">
                       <!-- text input -->
                       <div class="form-group">
-                        <label>Source</label>
-                        <input type="text" name="source" class="form-control" id ="source" value = "{{$inquiry->source}}">
-                      </div>
+                  <label for ="source">Source</label> 
+        <input type ="text" name="source" class="form-control" id ="source" value = "{{$inquiry->source}}">
+                    </div>
+                   
                     </div>
                     <div class="col-sm-6">
-                      <div class="form-group">
-                        <label>Type</label>
-                        <input type="text" name = "type"class="form-control" id ="type" value = "{{$inquiry->type}}">
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-sm-6">
-                      <div class="form-group">
-                        <label>Description</label>
-                        <input type="text" name = "description" class="form-control" id ="description"value = "{{$inquiry->description}}">
-                      </div>
-                    </div>
                     <div class="form-group">
-                        <label>Staff ID</label>
-                        <input type="text" name = "staff_id" class="form-control" id ="staff_id" value = "{{$inquiry->staff_id}}">
-                      </div>
+                  <label for ="type">Type</label> 
+        <input type ="text" name ="type" class="form-control" id ="type" value = "{{$inquiry->type}}">
+                    </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <!-- text input -->
                       <div class="form-group">
-                        <label>Student Id</label>
-                        <input type="text" name="student_id" class="form-control" id ="student_id" value = "{{$inquiry->student_id}}">
+                  <label for ="student_id">Student Id</label> 
+        <input type ="text" name="student_id" class="form-control" id ="student_id" value = "{{$inquiry->student_id}}">
                     </div>
+                   
+                    </div>
+                    <div class="col-sm-6">
                     <div class="form-group">
-                        <label>Date Time</label>
-                        <input type="text" name="date_time" class="form-control" id ="date_time" value = "{{$inquiry->date_time}}">
-                      </div>
+                  <label for ="description">Description</label> 
+        <input type ="text" name ="description" class="form-control" id ="description"value = "{{$inquiry->description}}">
                     </div>
-</div>
-
-
-                    
+                    </div>
+                    <div class="col-sm-6">
+                      <!-- text input -->
+                      <div class="form-group">
+                  <label for ="staff_id">Staff ID</label> 
+        <input type ="text" name="staff_id" class="form-control" id ="staff_id" value = "{{$inquiry->staff_id}}">
+                    </div>
+                   
+                    </div>
+                    <div class="col-sm-6">
+                    <div class="form-group">
+                  <label for ="date_time">Date Time</label> 
+        <input type ="datetime-local" name ="date_time" class="form-control" id ="date_time" value = "{{$inquiry->date_time}}">
                   
-      
+                    </div>
+                    </div>
+                    
+                    <!-- <div class="form-group">
+                  <label for ="staff_id">Staff ID</label> 
+        <input type ="text" name ="staff_id" class="form-control" id ="staff_id" value = "{{$inquiry->staff_id}}">
+                  </div>
+                    </div>
+                    
 
-                 
-                    <button type ="submit" value="Save" class="btn bg-gradient-success float-right">Save</button>
-</div>
+                    <div class="col-sm-6">
+                    <div class="form-group">
+                  <label for ="student_id">Student Id</label> 
+        <input type ="text" name ="student_id" class="form-control" id ="student_id" value = "{{$inquiry->student_id}}">
+                  </div>
 
-  
-  
-</form>
+                    </div>
+
+                    <div class="col-sm-6">
+                    <div class="form-group">
+                  <label for ="date_time">Date Time</label> 
+        <input type ="text" name ="date_time" class="form-control" id ="date_time" value = "{{$inquiry->date_time}}">
+                  </div>
+                    </div>
+                     -->
+              </form>
+              
+                  </div>
+
+                  <div class="card-footer">
+                  <button type="submit" value="Save" class="btn bg-gradient-success float-right ">save</button>
+                  <a href ="{{URL::previous()}}" class="btn bg-gradient-primary" href="#">Back</a>
+                </div>
 </div>
+</div>
+            
+
+               
+
+
+
+
+</body>
+</html>
  @endsection
+
 
 
