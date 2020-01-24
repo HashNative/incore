ee9f57bc7a436aaa93d4639f1751b11c670f1f70
fawzulhaq <fawzulhaq6@gmail.com>
2020-01-24 18:30:31 +0530

    student ttable updated

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -21,9 +21,7 @@
    
     <br>
     <br>
-   
    <section class="content">
-   
     <div class="row">
         <div class="col-12">
             <div class="card">
@@ -31,8 +29,7 @@
                     <h3 class="card-title">Student details</h3>
                 </div>
                 <!-- /.card-header -->
-                <div class="card-body ">
-              
+                <div class="card-body">
                     <table id="example1" class="table table-bordered table-striped">
 
                     <thead>
@@ -49,10 +46,10 @@
     <th>Inquiry Source</th> 
     <th>Transfer</th>  
     <th>Source</th>  
-    <th>Registration Date</th>  
+    <th> Date</th>  
     
     
-    <th style="width: 90px;">Actions</th> 
+    <th>Actions</th> 
     
      
 
@@ -81,33 +78,33 @@
            <td>
             <form action = "{{route('student.destroy',$student->id)}}" method = "POST" onsubmit="if(!confirm('Are you sure')){return false;}">
 
-           
+            <input type = "hidden" name = "_method" value = "delete"> 
+                    {{csrf_field()}}
                  
-                    
-                <a href="{{route('student.show',$student->id)}}" tittle="view details" class="btn btn-xs " style="color:#0066ff";>
-                    <i class="fa fa-eye"> </i>
-                </a>
-                
-                
-                <a href="{{route('student.edit',$student->id)}}" tittle="edit details" class="btn btn-xs " style="color:#ffc107";>
-                    <i class="fa fa-edit"> </i>
+            
+                <a href="{{route('student.show',$student->id)}}" tittle="view details" class="btn btn-sm " style="color:#0066ff";>
+                    <i class="fas fa-eye"> </i>
                 </a>
                 
                
-                <input type = "hidden" name = "_method" value = "delete"> 
-                    {{csrf_field()}}
-                    <button type="submit"  class="btn  btn-xs " style="color:#cc0000";><i tittle="delete details" class="fa fa-trash"></i></button>
+                <a href="{{route('student.edit',$student->id)}}" tittle="edit details" class="btn btn-sm " style="color:#ffc107";>
+                    <i class="fas fa-edit"> </i>
+                </a>
+                
+               
+                   
+                    <button type="submit"  class="btn  btn-sm " style="color:#cc0000";><i tittle="delete details" class="fa fa-trash"></i></button>
                 </form>
                 
            </td>
                 
     </tr>
     
-   
+    
    
     @endforeach
              </tbody>
-           
+            
             </table>
             </div>
             <!-- /.card-body -->
@@ -118,7 +115,6 @@
       </div>
       <!-- /.row -->
     </section>
-    
     <!-- /.content -->
 
 @endsection
